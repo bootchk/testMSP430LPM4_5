48436880ba20ef460778b55959e90fc1b4bfcd87
bootchk <bootch@nc.rr.com>
2019-03-06 08:17:28 -0500

    ISR_TRAP all interrupts.  Enable VMA.  Decode reset reasons.

--- src/main.c
+++ src/main.c
@@ -2,6 +2,8 @@
 
 #include <assert.h>
 
+#include <stdbool.h>
+
 
 
 void initAllGpioToOutputsLow()
@@ -70,9 +72,157 @@ void blinkGreenLed() {
 
 
 
+
+/*
+ * Clears all reset flags (iterates over the generator.)
+ *
+ * Returns true if one of the reasons is "wake from LPM5"
+ * There could be a concurrent other reason.
+ *
+ * Returns false if none of the reasons is "wake from LPM5."
+ * This is usually a cold restart (BOR) but could be the Reset pin or a SW reset (SWBOR)
+ *
+ * asserts false  for all other reset reasons
+ */
+bool isResetAWakeFromSleep() {
+  bool done = false;
+  bool result = false;
+
+  while (! done)
+  {
+    // not using even_in_range() which is just an optimization, and dissappeared from compiler?
+    unsigned int resetReason = SYSRSTIV;
+    switch (resetReason)
+    {
+    case SYSRSTIV_NONE:
+      done = true;  // stop loop - all reset reasons are cleared
+      break;
+
+    // Expected
+    case SYSRSTIV_LPM5WU:
+      result = true;
+      break;
+
+    // Expected
+    case SYSRSTIV_BOR:     // power up
+    case SYSRSTIV_RSTNMI:  // RST/NMI pin reset e.g. from debug probe
+    case SYSRSTIV_DOBOR:   // software initiated
+      break;
+
+    // Security. Accessing BSL that is protected. Probably errant
+    case SYSRSTIV_SECYV:     // Security violation
+       //assert(false);
+       break;
+
+     // WDT Time out
+     // Not expected since we stop WD
+     case SYSRSTIV_WDTTO:
+
+    // Software initiated POR
+    // But our software never initiates.
+    case SYSRSTIV_DOPOR:
+
+    // Faults, abnormal e.g. "bus error"
+    case SYSRSTIV_UBDIFG:    // FRAM Uncorrectable bit Error
+
+    // Keys
+    case SYSRSTIV_WDTKEY:    // WDT Key violation
+    case SYSRSTIV_FRCTLPW:   // FRAM Key violation
+
+    case SYSRSTIV_PERF:      // peripheral/config area fetch
+    case SYSRSTIV_PMMPW:     // PMM Password violation
+    case SYSRSTIV_FLLUL:     // FLL unlock
+
+    default:
+      assert(false);
+      break;
+    }
+  }
+  return result;
+}
+
+
+/*
+ * Returns true if any of the reset reasons are not awake from sleep.
+ * Awake from sleep may also be a concurrent reason.
+ *
+ * Returns false if the only reset reason is awake from sleep.
+ */
+bool isResetNotFromWake() {
+  bool done = false;
+  bool result = false;
+
+  while (! done)
+  {
+    // not using even_in_range() which is just an optimization, and dissappeared from compiler?
+    unsigned int resetReason = SYSRSTIV;
+    switch (resetReason)
+    {
+    case SYSRSTIV_NONE:
+      done = true;  // stop loop - all reset reasons are cleared
+      break;
+
+    // Expected
+    case SYSRSTIV_LPM5WU:
+      break;
+
+    // Expected
+    case SYSRSTIV_BOR:     // power up
+    case SYSRSTIV_RSTNMI:  // RST/NMI pin reset e.g. from debug probe
+    case SYSRSTIV_DOBOR:   // software initiated
+      result = true;
+      break;
+
+    // Security. Accessing BSL that is protected. Probably errant
+    case SYSRSTIV_SECYV:     // Security violation
+       //assert(false);
+       break;
+
+     // WDT Time out
+     // Not expected since we stop WD
+     case SYSRSTIV_WDTTO:
+
+    // Software initiated POR
+    // But our software never initiates.
+    case SYSRSTIV_DOPOR:
+
+    // Faults, abnormal e.g. "bus error"
+    case SYSRSTIV_UBDIFG:    // FRAM Uncorrectable bit Error
+
+    // Keys
+    case SYSRSTIV_WDTKEY:    // WDT Key violation
+    case SYSRSTIV_FRCTLPW:   // FRAM Key violation
+
+    case SYSRSTIV_PERF:      // peripheral/config area fetch
+    case SYSRSTIV_PMMPW:     // PMM Password violation
+    case SYSRSTIV_FLLUL:     // FLL unlock
+
+    default:
+      assert(false);
+      break;
+    }
+  }
+  return result;
+}
+
+
+
+
+
+
 int main(void)
 {
     WDTCTL = WDTPW | WDTHOLD;               // Stop WDT
+
+    // Clear NMI and VMA flags so we don't get an immediate interrupt if one has already occurred.
+    SFRIFG1 &= ~(NMIIFG | VMAIFG);
+
+    // vacant memory generate interrupt as well as read and execute funny
+    SFRIE1 |= VMAIE;
+
+    // BSL memory behave as vacant memory
+    SYSBSLC |= SYSBSLOFF;
+
 
     // RTC is not counting
     assert(RTCCTL == 0);
@@ -109,13 +259,18 @@ int main(void)
      */
 
     // Determine whether we are coming out of an LPMx.5 or a regular RESET.
+
     // Side effect of reading SYSRSTIV is to clear the highest priority reason (but not all of them?)
-    if (SYSRSTIV == SYSRSTIV_LPM5WU)        // MSP430 just woke up from LPMx.5
+    // if (SYSRSTIV == SYSRSTIV_LPM5WU)        // MSP430 just woke up from LPMx.5
+    //if (isResetAWakeFromSleep())
+    if (! isResetNotFromWake())
     {
         // Wake from LPM5
 
         // Example
         blinkRedLed();
+
+        // Port 1 IFG is set but we clear it later.
     }
     else
     {
@@ -126,7 +281,7 @@ int main(void)
     /*
      * Enable a source of a wake interrupt.
      */
-    enableButtonInterrupt();
+    enableButtonInterrupt();    // also clears the IFG before enabling
 
 
     /*
@@ -148,5 +303,27 @@ int main(void)
      */
     _low_power_mode_4();
 
+}
+
+
+/*
+ * catch unintended interrupts
+ */
+
+
+
+/*
+ * We don't expect any interrupt ISRs to be called.
+ *
+ * Even the button interrupt is not called (although we enable it.)
+ * See the design for waking from sleep.
+ */
+#pragma vector=   41, 42, 43, 44, 45, 46, \
+47, 48, 49, 50, 51, 52, \
+53, 54, 55, 56, 57, 58
+__interrupt void ISR_TRAP(void)
+{
+    blinkGreenLed();
+    // Since we did not clear the IFG, this will reoccur
 }
 
