b4c647f468383e20c8c4af4220215444ad68ba54
bootchk <bootch@nc.rr.com>
2019-03-15 16:06:15 -0400

    Build for MSP-EXP430FR6989

---
+++ DebugFR6989/src/main.d
@@ -0,0 +1,23 @@
+# FIXED
+
+src/main.obj: ../src/main.c
+src/main.obj: /home/bootch/ti/ccsv8/ccs_base/msp430/include/msp430.h
+src/main.obj: /home/bootch/ti/ccsv8/ccs_base/msp430/include/msp430fr6989.h
+src/main.obj: /home/bootch/ti/ccsv8/ccs_base/msp430/include/in430.h
+src/main.obj: /home/bootch/ti/ccsv8/tools/compiler/ti-cgt-msp430_18.1.5.LTS/include/intrinsics.h
+src/main.obj: /home/bootch/ti/ccsv8/tools/compiler/ti-cgt-msp430_18.1.5.LTS/include/intrinsics_legacy_undefs.h
+src/main.obj: /home/bootch/ti/ccsv8/tools/compiler/ti-cgt-msp430_18.1.5.LTS/include/assert.h
+src/main.obj: /home/bootch/ti/ccsv8/tools/compiler/ti-cgt-msp430_18.1.5.LTS/include/_ti_config.h
+src/main.obj: /home/bootch/ti/ccsv8/tools/compiler/ti-cgt-msp430_18.1.5.LTS/include/linkage.h
+src/main.obj: /home/bootch/ti/ccsv8/tools/compiler/ti-cgt-msp430_18.1.5.LTS/include/stdbool.h
+
+../src/main.c: 
+/home/bootch/ti/ccsv8/ccs_base/msp430/include/msp430.h: 
+/home/bootch/ti/ccsv8/ccs_base/msp430/include/msp430fr6989.h: 
+/home/bootch/ti/ccsv8/ccs_base/msp430/include/in430.h: 
+/home/bootch/ti/ccsv8/tools/compiler/ti-cgt-msp430_18.1.5.LTS/include/intrinsics.h: 
+/home/bootch/ti/ccsv8/tools/compiler/ti-cgt-msp430_18.1.5.LTS/include/intrinsics_legacy_undefs.h: 
+/home/bootch/ti/ccsv8/tools/compiler/ti-cgt-msp430_18.1.5.LTS/include/assert.h: 
+/home/bootch/ti/ccsv8/tools/compiler/ti-cgt-msp430_18.1.5.LTS/include/_ti_config.h: 
+/home/bootch/ti/ccsv8/tools/compiler/ti-cgt-msp430_18.1.5.LTS/include/linkage.h: 
+/home/bootch/ti/ccsv8/tools/compiler/ti-cgt-msp430_18.1.5.LTS/include/stdbool.h: 
